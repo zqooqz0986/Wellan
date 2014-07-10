44799a4249f238f827ceaeee7dcb16ef7b5e0ceb
zqooqz0986 <zqooqz0986@gmail.com>
2014-07-10 17:01:08 +0800

    支援分店

--- 伊莎貝爾輔銷SD/伊莎貝爾輔銷SD/I店員模組.cs
+++ 伊莎貝爾輔銷SD/伊莎貝爾輔銷SD/I店員模組.cs
@@ -9,12 +9,13 @@ namespace 伊莎貝爾輔銷SD
     public interface I店員模組
     {
         /// <summary>
-        /// 店員登入身分驗證
+        /// 店員登入身分驗證，含支援分店
         /// </summary>
-        /// <param name="id">店員帳號</param>
-        /// <param name="password">店員密碼</param>
+        /// <param name="編號">員工編號</param>
+        /// <param name="密碼">員工密碼</param>
+        /// <param name="服務地點">服務分店</param>
         /// <returns>身分驗證結果</returns>
-        身份認證 登入(string id, string password);
+        身份認證 登入(string 編號, string 密碼, string 服務地點);
 		
 		/// <summary>
         /// 查詢該分店所有店員(排除停用)，包含支援的店員
--- 伊莎貝爾輔銷SD/伊莎貝爾輔銷SD/店員.cs
+++ 伊莎貝爾輔銷SD/伊莎貝爾輔銷SD/店員.cs
@@ -1,3 +1,4 @@
+using DatabaseFoundation.Attribute;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,11 +6,21 @@ using System.Text;
 
 namespace 伊莎貝爾輔銷SD
 {
+    /// <summary>
+    /// BPSN(員工檔)
+    /// </summary>
     public class 店員
     {
+        /// <summary>
+        /// 員工編號(Key)
+        /// </summary>
+        [ColumnMapping("BPENO")]
 		public string 編號 { get; set; }
-		public string 名字 { get; set; }
-		public int 權限 { get; set; }
-		public string 分店 { get; set; }		
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        [ColumnMapping("BPNME")]
+		public string 名字 { get; set; }		
     }
 }

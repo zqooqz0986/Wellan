a57d68a410238a377b19801f703246b673feb2a1
zqooqz0986 <zqooqz0986@gmail.com>
2014-08-13 17:45:35 +0800

    console test

--- SqlServerTransaction/SecondTransaction/Program.cs
+++ SqlServerTransaction/SecondTransaction/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -10,22 +11,98 @@ namespace SecondTransaction
 {
     internal class Program
     {
+        private readonly static string Table1 = "dbo.LutestTable";
+
+        private readonly static string Table2 = "dbo.LutestSecondTable";
+
+        private readonly static string ConnectionString = "server=JOSEPH-LU;uid=sa;pwd=xj6u04vmp;database=IB00SS;";
+
+        private static SqlTransaction transaction = null;
+
         private static void Main(string[] args)
         {
-            using (var connection = new SqlConnection("server=JOSEPH-LU;uid=sa;pwd=xj6u04vmp;database=IB00SS;"))
+            using (var connection = new SqlConnection(ConnectionString))
             {
                 connection.Open();
-                using (var transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
+                using (transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
                 {
-                    var command = transaction.Connection.CreateCommand();
+                    //var result = Select(Table1, "1");
 
-                    command.CommandText = @"UPDATE [dbo].[LutestTable] SET [name] = '10' WHERE [id] = '1'";
-                    command.Transaction = transaction;
+                    Update(Table1, "1", "name1-Modified");
 
-                    var scalar = command.ExecuteNonQuery();
+                    //Insert(Table1, "1", "name1");
+
+                    //Delete(Table1, "1");
+
+                    Update(Table1, "2", "name2-Modified");
+
                     transaction.Commit();
                 }
             }
+        }
+
+        private static DataTable Select(string table, string id)
+        {
+            var command = transaction.Connection.CreateCommand();
+
+            command.CommandText = string.Format(@"SELECT * FROM {0} WHERE id = @p_id ", table);
+
+            command.Parameters.Add(new SqlParameter("@p_id", id));
+
+            command.Transaction = transaction;
+
+            var datatable = new DataTable();
+
+            var adapter = new SqlDataAdapter(command);
+
+            adapter.Fill(datatable);
+
+            return datatable;
+        }
+
+        private static void Insert(string table, string id, string name)
+        {
+            var command = transaction.Connection.CreateCommand();
+
+            command.CommandText = string.Format(@"INSERT INTO {0}
+                                                            ([id]
+                                                            ,[name])
+                                                    VALUES  (@p_id
+                                                            ,@p_name)", table);
+
+            command.Parameters.Add(new SqlParameter("@p_id", id));
+            command.Parameters.Add(new SqlParameter("@p_name", name));
+
+            command.Transaction = transaction;
+
+            command.ExecuteNonQuery();
+        }
+
+        private static void Update(string table, string id, string name)
+        {
+            var command = transaction.Connection.CreateCommand();
+
+            command.CommandText = string.Format(@"UPDATE {0} SET name = @p_name WHERE id = @p_id ", table);
+
+            command.Parameters.Add(new SqlParameter("@p_name", name));
+            command.Parameters.Add(new SqlParameter("@p_id", id));
+
+            command.Transaction = transaction;
+
+            command.ExecuteNonQuery();
+        }
+
+        private static void Delete(string table, string id)
+        {
+            var command = transaction.Connection.CreateCommand();
+
+            command.CommandText = string.Format(@"DELETE FROM {0} WHERE id = @p_id ", table);
+
+            command.Parameters.Add(new SqlParameter("@p_id", id));
+
+            command.Transaction = transaction;
+
+            command.ExecuteNonQuery();
         }
     }
 }
--- SqlServerTransaction/SqlServerTransaction/Program.cs
+++ SqlServerTransaction/SqlServerTransaction/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -10,22 +11,47 @@ namespace SqlServerTransaction
 {
     internal class Program
     {
+        private readonly static string Table1 = "dbo.LutestTable";
+
+        private readonly static string Table2 = "dbo.LutestSecondTable";
+
+        private readonly static string ConnectionString = "server=JOSEPH-LU;uid=sa;pwd=xj6u04vmp;database=IB00SS;";
+
+        private static SqlTransaction transaction = null;
+
         private static void Main(string[] args)
         {
-            using (var connection = new SqlConnection("server=JOSEPH-LU;uid=sa;pwd=xj6u04vmp;database=IB00SS;"))
+            using (var connection = new SqlConnection(ConnectionString))
             {
                 connection.Open();
-                using (var transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
+                using (transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
                 {
-                    var command = transaction.Connection.CreateCommand();
+                    var result = Select(Table1, "1");
 
-                    command.CommandText = @"SELECT Count(*) FROM LutestTable";
-                    command.Transaction = transaction;
+                    //var result = Select(Table1, "2");
 
-                    var scalar = command.ExecuteScalar();
                     transaction.Commit();
                 }
             }
+        }
+
+        private static DataTable Select(string table, string id)
+        {
+            var command = transaction.Connection.CreateCommand();
+
+            command.CommandText = string.Format(@"SELECT * FROM {0} WHERE id = @p_id ", table);
+
+            command.Parameters.Add(new SqlParameter("@p_id", id));
+
+            command.Transaction = transaction;
+
+            var datatable = new DataTable();
+
+            var adapter = new SqlDataAdapter(command);
+
+            adapter.Fill(datatable);
+
+            return datatable;
         }
     }
 }

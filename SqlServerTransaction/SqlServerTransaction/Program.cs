10393f487d22f1d0ea8e3f680323b9c842c3c14e
zqooqz0986 <zqooqz0986@gmail.com>
2014-08-13 14:24:19 +0800

    sql server transaction test solution

---
+++ SqlServerTransaction/SqlServerTransaction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlServerTransaction
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection("server=JOSEPH-LU;uid=sa;pwd=xj6u04vmp;database=IB00SS;"))
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    var command = transaction.Connection.CreateCommand();
+
+                    command.CommandText = @"SELECT Count(*) FROM LutestTable";
+                    command.Transaction = transaction;
+
+                    var scalar = command.ExecuteScalar();
+                    transaction.Commit();
+                }
+            }
+        }
+    }
+}

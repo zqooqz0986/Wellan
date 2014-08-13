ceceb498a9825f47d2f761b90fabd0e2a1ee7789
zqooqz0986 <zqooqz0986@gmail.com>
2014-08-13 14:31:40 +0800

    second transaction

---
+++ SqlServerTransaction/SecondTransaction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTransaction
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection("server=JOSEPH-LU;uid=sa;pwd=xj6u04vmp;database=IB00SS;"))
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    var command = transaction.Connection.CreateCommand();
+
+                    command.CommandText = @"UPDATE [dbo].[LutestTable] SET [name] = '10' WHERE [id] = '1'";
+                    command.Transaction = transaction;
+
+                    var scalar = command.ExecuteNonQuery();
+                    transaction.Commit();
+                }
+            }
+        }
+    }
+}

44456c7d551b0d5fc9fcd9d48c66c727db940eac
zqooqz0986 <zqooqz0986@gmail.com>
2014-09-05 15:06:11 +0800

    radiobuttonlist

---
+++ AppUserControl/AppUserControl/AppUserControl.Windows/RadioButtonList/RadioButtonList.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 使用者控制項項目範本已記錄在 http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace AppUserControl.RadioButtonList
+{
+    public sealed partial class RadioButtonList : UserControl
+    {
+        public RadioButtonList()
+        {
+            this.InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            // Add code to perform some action here.
+        }
+    }
+}
